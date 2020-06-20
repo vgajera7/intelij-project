41dcec202a7e7ee19d80af7432739dd8fd383e25
vgajera7 <vgajera7@gmail.com>
2020-06-20 12:16:47 +0530

    Java Code with Diff

--- src/DemoClass.java
+++ src/DemoClass.java
@@ -1,5 +1,5 @@
 public class DemoClass {
     public static void main(String[] args) {
-        System.out.println("This is my first Git IntelIJ project.");
+        System.out.println("This is my first Git IntelIJ project with Diff.");
     }
 }
